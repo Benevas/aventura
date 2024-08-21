a22ea6e87a87d87007b4d244daf6b42953b49274
Vinicius Henrique Benevento <169044594+Benevas@users.noreply.github.com>
2024-08-21 11:40:08 -0300

    aa

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const avanca = document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn');
 
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPasso = 'c-' + this.getAttribute('data-btn');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
